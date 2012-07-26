b93fa126cec97d8d4a094a5190fe7124d5417546
Brett Viren <bv@bnl.gov>
2012-07-26 15:22:43 -0400

    Move to bolted-on approach with the gnc example

---
+++ python/lcatr/gnc.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+'''
+Results for the Gain/Noise/ColdSpot (GNC) test.
+'''
+
+import schema as schema
+
+class GncPrimaryHDU(schema.PrimaryHDU):
+    def __init__(self, version = 0):
+        super(GncPrimaryHDU,self).__init__()
+        self.update_ext_name('GainNoiseColdSpot')
+        self.update_ext_version(version)
+        return
+    pass
+
+class GncInputFilesHDU(schema.TableHDU):
+    def __init__(self, filelist = None, sha1list = None, version = 0):
+        super(GncInputFilesHDU,self).__init__()
+        self.update_ext_name('FileRefs')
+        self.update_ext_version(version)
+        fc = schema.Column(name='FileName', format='A64', array=filelist)
+        hc = schema.Column(name='SHA1Hash', format='A64', array=sha1list)
+        self.columns = schema.ColDefs([fc,hc])
+        return
+    pass
+
+class GncGainsHDU(schema.TableHDU):
+    def __init__(self, lin_gains = None, med_gains = None, version = 0):
+        super(GncGainsHDU,self).__init__()
+        self.update_ext_name('Gains')
+        self.update_ext_version(version)
+        lc = schema.Column(name='LinGains', format='E', array=lin_gains)
+        mc = schema.Column(name='MedGains', format='E', array=med_gains)
+        self.columns = schema.ColDefs([lc,mc])
+        return
+    pass
+
+class GncNoiseHDU(schema.TableHDU):
+    def __init__(self, ovsc_noise = None, sdev_noise = None, version = 0):
+        super(GncNoisesHDU,self).__init__()
+        self.update_ext_name('Noises')
+        self.update_ext_version(version)
+        lc = schema.Column(name='OvScNois', format='E', array=ovsc_noise)
+        mc = schema.Column(name='SdevNois', format='E', array=sdev_noise)
+        self.columns = schema.ColDefs([lc,mc])
+        return
+    pass
+
+class GncColdSpotsHDU(schema.TableHDU):
+    def __init__(self, **kwds):
+        super(GncColdSpotsHDU,self).__init__()
+        self.update_ext_name('ColdSpot')
+        self.update_ext_version(kwds.get('version') or 0)
+        cols = []
+        for what in ['AmpNum','PixCount','SpotX','SpotY']:
+            c = schema.Column(name=what, format='I', array = kwds.get(what.lower()))
+            cols.append(c)
+            continue
+        self.columns = schema.ColDefs(cols)
+        return
+    pass
+        
+class GncResult(schema.HDUList):
+    def __init__(self):
+        super(GncResult,self).__init__(hdus = [
+                GncPrimaryHDU(),
+                GncInputFilesHDU(),
+                GncGainsHDU(),
+                GncColdSpotsHDU(),
+                ])
+        return
+    pass
+        
+
+
+if __name__ == '__main__':
+    gnc = GncResult()
+    print 'Validating empty GNC:'
+    gnc.validate()
+    gnc.info()
+    for hdu in gnc:
+        print hdu.header
+        print
+        continue
+
+
--- python/lcatr/schema.py
+++ python/lcatr/schema.py
@@ -1,301 +1,61 @@
 #!/usr/bin/env python
 '''
-Describe the schema for LSST CCD Acceptance Testing Result files.
+Wrap pyfits classes to support LCATR schema.
 
+This wrapping adds the following methods to each HDU and HDUList classes
 
-See the schema.org document for details. 
-
- - /result/ :: \to (/PrimaryHDU/, /FragmentHDU/, ...)
- - /PrimaryHDU/ :: \to /HDU/
- - /FragmentHDU/ :: \to /HDU/
- - /HDU/ :: \to (/header/, /payload/)
- - /payload/ :: \to either a /table/ or /data/
- - /header/ :: \to (/card/, /card/, ...)
- - /table/ :: \to (/column/, /column/, ...)
- - /data/ :: \to (t.b.d.)
- - /card/ :: \to (/name/, /typeobj/, /comment/, /description/)
- - /column/ :: \to (/name/, /typeobj/, /description/)
- - /name/ :: \to a string, 8 characters or less
- - /typeobj/ :: \to an instance of a Python class representing the type of the element
- - /comment/ :: \to a short FITS comment
- - /description/ :: \to a free form, comprehensive description of the element
+ - a validate()         raise an exception if the HDU is invalid
 
 '''
 
-import numpy
-import hashlib
-import pyfits
+from pyfits import *
 
-class Base(object): 
+def PrimaryHDU_validate(self):
     '''
-    Base for schema classes.  
+    Validate a Primary HDU.
     '''
+    self.verify()
+    for name in ['EXTNAME','EXTVER']: self.header[name]
+    return
+PrimaryHDU.validate = PrimaryHDU_validate
 
-    def validate(self):
-        '''
-        Validate the contents of the object.
-        '''
-        not_implemented()
-        return
 
-    def fits(self):
-        '''
-        Return the pyfits object corresponding to self.
-        '''
-        return None
-
-    def __str__(self):
-        return 'NotImplemented'
-
-    pass
-
-class Result(Base):
-    def __init__(self, primary_hdu, fragment_hdu_list):
-        self.hdu = [primary_hdu] + fragment_hdu_list
-        return
-    def __str__(self):
-        ret = ['Result: %d HDUS:'%len(self.hdu)] + [str(hdu) for hdu in self.hdu]
-        return '\n'.join(ret)
-
-    def fits(self):
-        return pyfits.HDUList([hdu.pyfits() for hdu in self.hdu])
-
-    pass
-
-class HDU(Base):
-    def __init__(self, header, payload):
-        self.header = header
-        self.payload = payload or list()
-        return
-
-    def __str__(self):
-        ret = ['[0x%x] HDU (%s): ' % (id(self), self.name())]
-        ret.append(str(self.header))
-        ret += [str(p) for p in self.payload]
-        return '\n'.join(ret)
-
-    def name(self):
-        '''
-        Return the canonical name for this HDU
-        '''
-        return self.header.name()
-        
-    def validate(self):
-        self.header.validate()
-        for p in self.payload:
-            p.validate()
-        return
-
-    def fits(self):
-        fits_header = self.header.fits()
-        fits_payload = [p.fits() for p in self.payload]
-        return pyfits.new_table(fits_payload, header = fits_header)
-
-    pass
-
-class TableHDU(HDU):
-    def __init__(self, header, columns = None):
-        self.base = super(TableHDU,self)
-        self.base.__init__(header)
-        if columns is None: columns = list()
-        self.columns = columns
-        return
-
-    def validate(self):
-        self.base.validate()
-        for c in self.columns():
-            c.validate()
-            continue
-        return
-
-    def __str__(self):
-        ret = [str(self.base)]
-        for c in self.columns():
-            ret.append(str(c))
-        return '\n'.join(ret)
-
-    def fits(self):
-
-    pass
-
-class Header(Base):
-    required_cards = ['EXTNAME','CHECKSUM','DATASUM','SCHEMAV']
-
-    def __init__(self, cards = None):
-        if cards is None: cards = list()
-        self.cards = cards
-        return
-
-    def __str__(self):
-        ret = ['[0x%x] Header: %d cards:' % (id(self),len(self.cards))] 
-        ret += [str(c) for c in self.cards]
-        return '\n'.join(ret)
-
-    def validate(self):
-        cnames = []
-        for c in self.cards:
-            c.validate()
-            cnames.append(c)
-
-        for needed in Header.required_cards:
-            if needed not in cnames:
-                raise ValueError, 'Lack required card "%s"' % needed
-            continue
-        return
-
-    def card(self, cardname):
-        for c in self.cards:
-            if c.name == cardname:
-                return c
-            continue
-        return None
-
-    def name(self):
-        'Return name (EXTNAME) of this header'
-        nc = self.card('EXTNAME')
-        if not nc: return None
-        return nc.value
-
-    pass
-
-class Table(Base):
-    def __init__(self, columns = None):
-        self.set(columns)
-        return
-
-    def __str__(self):
-        ret = ['[0x%x] Table (%s):' % (id(self),len(self.columns))]
-        ret += [str(c) for c in self.columns]
-        return '\n'.join(ret)
-
-    def set(self, columns):
-        if columns is None: columns = list()
-        self.columns = columns
-        return
-
-    def validate(self):
-        if not self.columns:
-            raise ValueError, 'Table: no columns'
-        for c in self.columns:
-            c.validate()
-        return
-
-    pass
-
-class Data(Base):
-    def __init__(self):
-        return
-    pass
-
-class TypeObj():
+def TableHDU_validate(self):
     '''
-    A type.
+    Validate a TableHDU
     '''
-
-    tofits = {
-        'int16':'I',
-        'int32':'J',
-        'int64':'K',
-        'float32':'E',
-        'float64':'D',
-        'sha1':'A64',
-        'str':'A64',
-        'datetime': 'A86',
-        }
-
-    def __init__(self, typestr):
-        self.typestr = typestr
-        return
-
-    def __str__(self):
-        return self.typestr
-
-    def fits(self):
-        return TypeObj.tofits(self.typestr)
-
-    def pytype(self):
-        if 'sha1' == self.typestr:
-            return type(hashlib.sha1())
-        if 'str' == self.typestr:
-            return str
-        return numpy.typeDict.get(self.typestr)
+    self.verify()
+    for name in ['EXTNAME','EXTVER']: self.header[name]
+    if not len(self.columns):
+        raise ValueError,'TableHUD "%s" with no columns'%self.name
+    return
+TableHDU.validate = TableHDU_validate
 
 
-    def istype(self,obj):
-        t = self.pytype()
-        if not t: return None
-        return isinstance(obj,t)
-        
-    def strval(self, obj):
-        if not self.istype(obj): 
-            raise TypeError, '%s not of type %s, it is %s' % (str(obj),str(self),type(obj))
-        if self.typestr == 'sha1':
-            return obj.hexdigest()
-        if self.typestr == 'datetime':
-            return obj.strftime("%Y-%m-%dT%H:%M:%S")
-        return str(obj)
-    pass
+def HDUList_validate(self):
+    '''
+    Validate a list of HDUs
+    '''
+    self.verify()
+    for hdu in self:
+        hdu.validate()
+    return
+HDUList.validate = HDUList_validate
 
-class Card(Base):
-    def __init__(self, name, typestr, comment, description, value = None):
-        self.name = name
-        self.typeobj = TypeObj(typestr)
-        self.comment = comment
-        self.description = description
-        self.set(value)
-        return
+def test_PrimaryHDU():
+    p = pyfits.PrimaryHDU()
+    try:
+        p.validate()
+    except KeyError,msg:
+        print msg
+        print 'as expected'
+    else:
+        raise RuntimeError,'validate did not fail on empty PrimaryHDU'
 
-    def set(self, value):
-        self.value = value
-        return
+    p.update_ext_name('TestResult')
+    p.update_ext_version(0)
+    p.validate()                # should not raise exception
+    print p.header
 
-    def __str__(self):
-        ret = '[0x%x] Card: %s (%s): %s' % (id(self), self.name, self.typeobj, self.description)
-        if not self.value is None: ret += ' = "%s"' % self.typeobj.strval(self.value)
-        return ret
-
-    def validate(self):
-        if not all((self.name,self.typeobj,self.comment,self.description, self.value)):
-            raise ValueError, 'Card not fully formed.'
-        if not self.typeobj.istype(self.value):
-            raise TypeError, 'type "%s" not compatible with "%s"' % \
-                (str(self.typeobj),str(self))
-        return
-
-
-    pass
-
-class Column(Base):
-    def __init__(self, name, typeobj, description, array = None):
-        self.name = name
-        self.typeobj = TypeObj(typeobj)
-        self.description = description
-        self.set(array)
-        return
-
-    def __str__(self):
-        ret = '[0x%x] Column: %s %s: %s' % (id(self),self.name, self.typeobj, self.description)
-        if self.array: 
-            sar = [self.typeobj.strval(a) for a in self.array]
-            ret += ' = %s' % sar
-        return ret
-
-    def set(self, array):
-        self.array = array
-        return
-
-    def fits(self):
-        'Return a FITS pyfits.Column'
-        return pyfits.Column(name = self.name, format = self.typeobj.fits(), array = self.aray)
-    
-    def validate(self):
-        if not all((self.name,self.typeobj,self.description,self.array)):
-            raise ValueError, 'Column not fully formed.'
-        if not all([self.typeobj.istype(a) for a in self.array]):
-            raise ValueError, 'Column "%s" not of type "%s"' % \
-                (self.name, self.typeobj)
-        return
-
-    pass
-
-
+if __name__ == '__main__':
+    test_PrimaryHDU()
